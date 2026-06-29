4eb2005c6e842e80042b6438e84da4772ef84147
LamThanh2k04 <ngolam2k04@gmail.com>
2026-06-29 20:39:42 +0700

    chore(main): add provider at main use redux

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import { store } from './app/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* 
+    Provider là một component của react-redux
+    Nhiệm vụ: đưa store của redux xuống toàn bộ ứng dụng react thì những component khác sẽ có thể truy cập store bằng useSelector và useDispatch để lấy dữ liệu
+    */}
+  <Provider store={store}>
+      <App />
+  </Provider>
   </StrictMode>,
 )
