8b7cc27f25f03564bbb4fdeb67396764e512357b
LamThanh2k04 <ngolam2k04@gmail.com>
2026-07-03 16:24:56 +0700

    feat(AppRouter): add router role admin

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -4,12 +4,20 @@ import HomePage from '../pages/HomePage';
 import MainLayout from '../layouts/MainLayout';
 import LoginPage from '../pages/LoginPage';
 import RegisterPage from '../pages/RegisterPage';
+import DashboardAdminPage from '../pages/admin/DashboardAdminPage';
+import AdminRouter from './AdminRouter';
+import AdminLayout from '../layouts/AdminLayout';
 const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
+                <Route element={<AdminRouter/>}>
+                    <Route element={<AdminLayout />} >
+                        <Route path="/admin" element={<DashboardAdminPage />} />
+                    </Route>
+                </Route>
                 <Route element={<MainLayout />}>
                     <Route path="/" element={<HomePage />} />
                 </Route>
