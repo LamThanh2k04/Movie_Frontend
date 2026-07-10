a885bf2aa41c536ceab664034ac8d4c936dec469
LamThanh2k04 <ngolam2k04@gmail.com>
2026-07-10 15:02:42 +0700

    feat(userApi): create update and update status

--- src/services/admin/userApi.js
+++ src/services/admin/userApi.js
@@ -1,7 +1,14 @@
 import api from "../axios"
 
-export const createUser =  (data) => {
+export const createUser = (data) => {
     return api.post('/user/createUser', data)
+}
+export const updateUser = (userId, data) => {
+    return api.put(`/user/updateUser/${userId}`, data)
+}
+
+export const updateUserStatus = (userId) => {
+    return api.put(`/user/updateUserStatus/${userId}`)
 }
 export const getAllUsers = (params) => {
     return api.get('/user/getAllUsers', {
