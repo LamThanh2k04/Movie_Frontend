fd2fae8d4042892fb547ca6873fe394bfa26d6e3
LamThanh2k04 <ngolam2k04@gmail.com>
2026-07-10 14:56:48 +0700

    refector(search): add icon search

--- src/components/admin/UserSearch.jsx
+++ src/components/admin/UserSearch.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-
+import { Search } from 'lucide-react';
 const UserSearch = ({ search, setSearch }) => {
     return (
-        <div>
+        <div className='relative w-full max-w-sm'>
+            <Search className='absolute left-3 top-1/2 -translate-y-1/2 text-gray-500'/>
             <input
                 type="text"
                 placeholder="Tìm kiếm người dùng..."
                 value={search}
                 onChange={(e) => setSearch(e.target.value)}
-                className="w-full max-w-sm px-4 py-2 rounded-lg bg-zinc-900 border border-zinc-700 text-white placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-red-500"
+                className="w-full pl-10 pr-4 py-2 rounded-lg bg-zinc-900 border border-zinc-700 text-white placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-red-500"
             />
         </div>
     )
