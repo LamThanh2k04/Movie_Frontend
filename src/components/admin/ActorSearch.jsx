94bc0fd1a192e5c5215cbfc8b95c81accc5f7ea2
LamThanh2k04 <ngolam2k04@gmail.com>
2026-07-15 10:42:03 +0700

    feat(actor): implement pagination search and table

---
+++ src/components/admin/ActorSearch.jsx
@@ -0,0 +1,19 @@
+import { Search } from 'lucide-react'
+import React from 'react'
+
+const ActorSearch = ({search,setSearch}) => {
+   return (
+        <div className='relative w-full max-w-sm'>
+            <Search className='absolute left-3 top-1/2 -translate-y-1/2 text-gray-500' />
+            <input
+                type="text"
+                placeholder="Tìm kiếm diễn viên..."
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                className="w-full pl-10 pr-4 py-2 rounded-lg bg-zinc-900 border border-zinc-700 text-white placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-red-500"
+            />
+        </div>
+    )
+}
+
+export default ActorSearch
