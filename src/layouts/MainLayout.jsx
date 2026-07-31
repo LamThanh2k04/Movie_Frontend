4001e34449ad97c6975087c1a5ef8c427df62901
LamThanh2k04 <ngolam2k04@gmail.com>
2026-07-31 12:20:13 +0700

    feat(MainLayout): props data into Header and context into Outlet and fix UI shared

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header'
+import useSearch from '../hooks/useSearch'
+import Footer from '../components/Footer'
+// outlet hk phải component mình tạo nên hk dùng cách props đc 
+// vì hk biết bên trong nó chứa home hay là gì nên hk dùng cách props
 const MainLayout = () => {
+    const {search, setSearch} = useSearch()
     return (
-        <div>
-            <Header/>
-            <Outlet/>
-            <h2>Footer</h2>
+        <div className = 'text-white bg-black min-h-screen'>
+            <Header
+            search={search}
+            setSearch={setSearch}
+            />
+            
+            <Outlet 
+            context={{search}}
+            />
+            <Footer/>
         </div>
     )
 }
